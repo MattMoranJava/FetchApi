73fc0e756264b00c4defc8e1f50616d42a7fe23d
MattMoranJava <Sciamachy@gmail.com>
2018-07-10 09:19:32 +0100

    Stripping extraneous braces

--- app.js
+++ app.js
@@ -5,35 +5,35 @@ document.getElementById('button3').addEventListener('click', getExternal);
 //get Local text file data
 function getText(){
     fetch('test.txt')
-    .then((response) => response.text())
-    .then((data) => document.getElementById('output').innerHTML = data)
-    .catch((err) => console.log(err));
+    .then(response => response.text())
+    .then(data => document.getElementById('output').innerHTML = data)
+    .catch(err => console.log(err));
 };
 
 // get local JSON data
 function getJson(){
     fetch('posts.json')
-    .then((response) => response.json())
-    .then((data) => {
+    .then(response => response.json())
+    .then(data => {
         let output = '<ol>';
-        data.forEach((post) => output += `<li>${post.title}</li>`);
+        data.forEach(post => output += `<li>${post.title}</li>`);
         output += '</ol>';
         document.getElementById('output').innerHTML = output;
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 };
 
 // Get API Data
 function getExternal(){
     fetch('https://api.github.com/users')
-    .then((response) => response.json())
-    .then((data) => {
+    .then(response => response.json())
+    .then(data => {
         console.log(data);
         
         let output = '<ul>';
-        data.forEach((user) => output += `<li>${user.login}</li>`);
+        data.forEach(user => output += `<li>${user.login}</li>`);
         output += '</ul>';
         document.getElementById('output').innerHTML = output;
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 };
